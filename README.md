c3604162f5a148a73316a66ea764a152e51a583e
Pavel Zubko <tyyrok@gmail.com>
2023-06-17 21:56:14 +0400

    initial commit for the challenge solution

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Port Scanner
 
+This is the solution for the FreeCodeCamp challenge based on the boilerplate below
+
 This is the boilerplate for the Port Scanner project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/information-security/information-security-projects/port-scanner
--- port_scanner.py
+++ port_scanner.py
@@ -1,8 +1,21 @@
+#!/usr/bin/python3
+
 import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(5)
 
 def get_open_ports(target, port_range):
     open_ports = []
 
-
+    for port in range(port_range[0], port_range[1] + 1):
+        print(target, port)
+        if not s.connect_ex((target, port)):
+            open_ports.append(port)
+        else:
+            print("Closed")
 
     return(open_ports)
+
+print(get_open_ports("209.216.230.240", [440, 445]))
+#print(get_open_ports("www.stackoverflow.com", [79, 82]))
